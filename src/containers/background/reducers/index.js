59ef4c1fa8bab4609a5c79724f18f902bdf8ca67
Philip Chwistek <mrsportsnerd@gmail.com>
2020-02-11 14:52:59 -0800

    added initial UI for table row

--- src/containers/background/reducers/index.js
+++ src/containers/background/reducers/index.js
@@ -1,13 +1,12 @@
 
 import {combineReducers} from 'redux'
 import user from './user'
-import popup from './popup'
+// import popup from './popup'
 import tabs from './tabs'
 import search from './search'
 
 export default combineReducers({
   user,
-  popup,
   tabs,
   search,
 })
--- src/containers/popup/Table/Table.jsx
+++ src/containers/popup/Table/Table.jsx
@@ -1,11 +1,77 @@
 import React from 'react'
 import SortBar from './SortBar'
+import Button from '../../common-ui/Button'
+
+const rows = [
+  {
+    name: 'React Development',
+    author: 'JubJubTumTum',
+    tags: [
+      'react',
+      'webdevelopment',
+      'fast'
+    ],
+    attributes: [
+      'Popular', 
+      'Favorite'
+    ],
+    usedBy: '1.4K',
+    favorites: '400'
+  }
+]
+
+function getSrc(attribute) {
+
+  switch(attribute){
+    case 'Popular': 
+      return '../../../assets/fire.png'
+    case 'Favorite':
+      return '../../../assets/star1.png'
+  }
+
+
+}
 
 export default function Table() {
   return (
     <div>
       <SortBar title={ 'All' }/>
-      This is the table
+      <div className={ 'table__container' }>
+        {
+          rows.map(row => (
+            <div className={'table__row'}>
+              <div className={ 'table__row__title '}>
+                { row.name }
+                <div className={ 'table__row__attributes '}>
+                  {
+                    row.attributes.map(attrib => (
+                      <div className={ 'tooltip' }>
+                        <img src={ getSrc(attrib) } className={ `table__row__attribute-icon` }/>
+                        <span class="tooltiptext"> { attrib }</span>
+                      </div>
+                    ))
+                  }
+                </div>
+              </div>
+              <div className={ 'table__row__author '}>
+                { row.author } shared 3 months ago
+              </div>
+              <div className={ 'table__row__tags'}>
+              {
+                row.tags.map(tag => (
+                  <div className={ 'tag-result' }>
+                    { tag }
+                  </div>
+                ))
+              }
+              </div>
+              <div className={ 'table__row__launch' }>
+                <Button text={ 'Launch' } type={ 'secondary' } />
+              </div>
+            </div>
+          ))
+        }
+      </div>
     </div>
   )
 }
