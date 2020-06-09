b89395cc6e0de8f70de715ec26bfd467658b0532
Philip Chwistek <mrsportsnerd@gmail.com>
2020-06-08 22:42:20 -0400

    test fix

--- src/containers/popup/Menu/Menu.jsx
+++ src/containers/popup/Menu/Menu.jsx
@@ -50,7 +50,7 @@ const Menu = (props) => {
             <div>
               <img src={ stewLogo } className={ 'menu__body__logo' } />
             </div>
-            version 0.5
+            Version 1.0.0
             <div className='menu__body__switches'>
               <div className='menu__body__switch-container'>
                 <label>
